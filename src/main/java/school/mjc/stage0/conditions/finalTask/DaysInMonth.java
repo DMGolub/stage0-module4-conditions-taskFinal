4c4b5f1e6330f05db1f72b8db29164a6dd29449a
Dmitry Golub <dmgolub@mail.ru>
2022-12-24 23:31:33 +0400

    Task completed.

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,30 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (year < 0 || month < 1 || month > 12) {
+            System.out.println("invalid date");
+        } else {
+            if (isLeapYear(year) && month == 2) {
+                System.out.println(29);
+            } else {
+                int days = daysInMonth(month);
+                System.out.println(days);
+            }
+        }
 
+    }
+
+    private boolean isLeapYear(int year) {
+        return year % 100 == 0 && year % 400 == 0 || year % 100 != 0 && year % 4 == 0;
+    }
+
+    private int daysInMonth(int month) {
+        int days = 0;
+        switch (month) {
+            case 2 -> days = 28;
+            case 1, 3, 5, 7, 8, 10, 12 -> days = 31;
+            case 4, 6, 9, 11 -> days = 30;
+        }
+        return days;
     }
 }
